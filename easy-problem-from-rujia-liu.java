125dea842e26bfa91d1f2c74835f9bb884ee55c6
DonatoM <cruzmdonato@gmail.com>
2015-02-06 03:06:09 -0500

    Solution for UVA problem 11991. Still get a TLE. Will most likely need to implement it in C++ to meet the 1000MS limit.

--- easy-problem-from-rujia-liu.java
+++ easy-problem-from-rujia-liu.java
@@ -2,18 +2,45 @@ import java.io.*;
 import java.util.*;
 
 
-public class ProblemThree {
+public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String line;
-		HashMap<Integer,ArrayList<Integer>> num_to_indexes = new HashMap<Integer,ArrayList<Integer>>(1000000);
-		
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		List[] num_to_indexes = new ArrayList[1000001];
+
+        	for (int i = 0; i < 1000001; i++) {
+            		num_to_indexes[i] = new ArrayList<Integer>();
+        	}
+        
 		while((line = in.readLine()) != null){
 			String[] tokens = line.split(" ");
 			int array_length = Integer.parseInt(tokens[0]);
 			int num_of_cases = Integer.parseInt(tokens[1]);
-			String[] numbers = in.readLine().split(" ");
+			
+			StringTokenizer st = new StringTokenizer(in.readLine()," ");
+			
+			int x = 0;
+			while (st.hasMoreTokens()) {
+				num_to_indexes[Integer.parseInt(st.nextToken())].add(x);
+			    x++;
+			}
+			
+			int[] results = new int[num_of_cases];
+			
+			for(int i = 0; i < num_of_cases; i++){
+				String[] test = in.readLine().split(" ");
+				int index = Integer.parseInt(test[0]);
+				int value = Integer.parseInt(test[1]);
+				
+				if(index > num_to_indexes[value].size())
+					results[i] = 0;
+				else
+					results[i] = (Integer)num_to_indexes[value].get(index-1) + 1;
+			}
+			
+			for (Integer element : results) {
+			    System.out.println(element);
+			}
 		}
 	}
 
