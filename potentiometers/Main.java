eeb18826e9100aa19f78858138522a128a893bc9
DonatoM <cruzmdonato@gmail.com>
2015-02-15 22:31:10 -0500

    Added the skeleton code.

---
+++ potentiometers/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String [] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		
+		while((line = in.readLine()) != null){
+			String[] tokens = line.split(" ");
+		}
+	}
+}
