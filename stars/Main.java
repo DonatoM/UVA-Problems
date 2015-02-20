9a105c5a7bc7c9b8ce6bbd206d27963aea5ce9ca
DonatoM <cruzmdonato@gmail.com>
2015-02-20 09:10:26 -0500

    Added solution to Timus Stars problem.

--- stars/Main.java
+++ stars/Main.java
@@ -7,7 +7,56 @@ public class Main {
 		String line;
 		
 		while((line = in.readLine()) != null){
-			String[] tokens = line.split(" ");
+			int number_of_stars = Integer.parseInt(line);
+			int[] star_level_count = new int[number_of_stars];
+			Fenwick ft = new Fenwick(32000);
+			
+			for(int i = 0; i < number_of_stars; i++){
+				String[] coordinates = in.readLine().split(" ");
+				int x_cordinate = Integer.parseInt(coordinates[0]) + 1;
+				int star_level = ft.sumQuery(x_cordinate);
+				ft.adjust(x_cordinate, 1);
+				star_level_count[star_level] += 1;
+			}
+			
+			for(int count: star_level_count)	System.out.println(count);
 		}
+	}
+}
+
+class Fenwick {
+	public int[] table;
+
+	public Fenwick(int maxN) {
+		this.table = new int[maxN + 2];
+	}
+	
+	public void set(int index, int value){
+		int diff = value - getValue(index);
+		adjust(index,diff);
+	}
+
+	public int sumQuery(int a, int b) {
+		return sumQuery(b) - sumQuery(a - 1);
+	}
+
+	public int sumQuery(int k) {
+		int ret = 0;
+		while (k > 0) {
+			ret += table[k];
+			k &= k - 1;
+		}
+		return ret;
+	}
+
+	public void adjust(int i, int adj) {
+		while (i < table.length) {
+			table[i] += adj;
+			i += (i & (-i));
+		}
+	}
+
+	public int getValue(int i) {
+		return sumQuery(i, i);
 	}
 }
