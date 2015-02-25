c68b8cdde86d50d54d87cba87f1376e34ece3490
DonatoM <cruzmdonato@gmail.com>
2015-02-24 21:03:33 -0500

    Added skeleton for UVA problem 357.

---
+++ let_me_count_the_ways/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class LetMeCountTheWays {
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		while((line = in.readLine()) != null){
+		}
+	}
+}
