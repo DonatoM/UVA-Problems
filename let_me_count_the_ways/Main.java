f6a915255ad615a18ce35ab1795d37eda1c6c467
DonatoM <cruzmdonato@gmail.com>
2015-02-26 00:28:58 -0500

    Added recursive solution. Need to test if I exceed the TL.

--- let_me_count_the_ways/Main.java
+++ let_me_count_the_ways/Main.java
@@ -1,14 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-public class LetMeCountTheWays {
+public class Main {
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		String line;
-		while((line = in.readLine()) != null){
+		StringBuilder answer = new StringBuilder();
+		int[] coins = {1,5,10,25,50};
+		int size = coins.length;
+		
+		while(!(line = in.readLine()).isEmpty()){
+			int value = Integer.parseInt(line);
+			int ways = count(coins, size, value);
+			if(ways > 1) answer.append("There are " + ways + " ways to produce " + value + " cents change.\n");
+			else if(ways == 1) answer.append("There is only 1 way to produce " + value + " cents change.\n");
 		}
+		answer.setLength(answer.length() - 1);
+		System.out.println(answer);
 	}
-	public int count(int[] values, int coins, int sum){
-		return 0;
+	
+	public static int count(int[] coins, int num_of_coins, int value){
+		if (value == 0) return 1;
+		if (value < 0) return 0;
+		if (num_of_coins <= 0 && value >= 1) return 0;
+		return count(coins, num_of_coins, value-coins[num_of_coins-1]) + count(coins, num_of_coins - 1, value);
 	}
 }
