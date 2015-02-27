cca2b533a7de4a1c9a8e89c02aa9a7a05865a90b
DonatoM <cruzmdonato@gmail.com>
2015-02-26 23:43:24 -0500

    Added working solution but somehow ran into a run time error.

--- let_me_count_the_ways/Main.java
+++ let_me_count_the_ways/Main.java
@@ -8,22 +8,23 @@ public class Main {
 		String line;
 		StringBuilder answer = new StringBuilder();
 		int[] coins = {1,5,10,25,50};
-		int size = coins.length;
+		long[] cashier = new long[30050];
+		cashier[0] = 1;
+				
+		for(int i = 0; i < 5; i++){
+			for(int j = coins[i]; j <= 30010; j++){
+				cashier[j] += cashier[j - coins[i]];
+			}
+		}
 		
 		while(!(line = in.readLine()).isEmpty()){
-			int value = Integer.parseInt(line);
-			int ways = count(coins, size, value);
-			if(ways > 1) answer.append("There are " + ways + " ways to produce " + value + " cents change.\n");
-			else if(ways == 1) answer.append("There is only 1 way to produce " + value + " cents change.\n");
+			int index = Integer.parseInt(line);
+			if(cashier[index] > 1) answer.append("There are " + cashier[index] + " ways to produce " + index + " cents change.\n");
+			else if(cashier[index] == 1) answer.append("There is only 1 way to produce " + cashier[index] + " cents change.\n");
 		}
+		
 		answer.setLength(answer.length() - 1);
 		System.out.println(answer);
 	}
-	
-	public static int count(int[] coins, int num_of_coins, int value){
-		if (value == 0) return 1;
-		if (value < 0) return 0;
-		if (num_of_coins <= 0 && value >= 1) return 0;
-		return count(coins, num_of_coins, value-coins[num_of_coins-1]) + count(coins, num_of_coins - 1, value);
-	}
 }
+
