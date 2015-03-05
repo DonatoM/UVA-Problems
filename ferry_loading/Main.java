319e848f3a5683732b3625d28c4dd438278181fe
DonatoM <cruzmdonato@gmail.com>
2015-03-04 22:20:29 -0500

    Continued working on ferry_loading uva problem.

--- ferry_loading/Main.java
+++ ferry_loading/Main.java
@@ -1,13 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String line;
 		
-		while((line = in.readLine()) != null){
-			int test_cases = Integer.parseInt(line);
+		int num_cases = Integer.parseInt(in.readLine());
+		
+		for(int i = 0; i < num_cases; i++){
+			in.readLine();
+			
+			int ferry_length = Integer.parseInt(in.readLine()) * 100;
 		}
+		
 	}
 }
